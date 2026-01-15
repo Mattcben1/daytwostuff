0d878889d5ff132bb5bd2f36f25fd8eded10755d
Mattcben1 <bxp6yd@virginia.edu>
2026-01-15 22:46:55 +0000

    modified daytwo.py

--- daytwo.py
+++ daytwo.py
@@ -1,1 +1,25 @@
 #%%
+# @Mattcben1 ➜ /workspaces/daytwostuff (main)
+#%%
+# Yes to make sure it follows and satisfies the requirements.txt file
+#%%
+# (.venv) @Mattcben1 ➜ /workspaces/daytwostuff (main) _ Yes it shows I'm in the venv
+#%%
+import pandas as pd 
+data = pd.read_csv('pitch-types.csv')
+data.head()
+#%%
+# For the extensions menu, I've noticed it's simplistic and serves an important purpose in implementing additional functions to base software.
+#%%
+# 1. Key organizational features 2. Auto-generating pandas code based on data needs. 3. Integration with other data analysis tools.
+#%%
+# Plotly Version: 6.5.1
+#%% 
+# A requirements.txt file is essential for tracking dependencies in a project, ensuring that anyone working on the project can install the exact versions of libraries needed for compatibility and functionality.
+#%%
+# Recipe for getting into a new environment and using codespace
+# 1. Fork the repo you intend to use, or create one
+# 2. From there, navigate to the green code button in your repo, click codespaces, and follow it to allow opening of a codespace
+# 3. You'll be prompted to the VS web browser, but it's important that you access it locally --> File -> New Window -> Connect to Codespaces...You should have ready to be accessable.
+# 4. Ensure you download the neccessaey packages (Ipykernal, python env., jupyter) if you need them
+# 5. Create and activate a virtual environment and install all local dependencies (requirements.txt, etc.)
